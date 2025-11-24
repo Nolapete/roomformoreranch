c667ce323a8fcc2689de3ad835321129e2111b23
Pete LoGiudice <pete.logiudice@zohomail.com>
2025-11-23 23:10:27 -0600

    Minor fixes

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
+from django.apps import apps
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import RedirectView
-from django.apps import apps
 
 admin.site.site_header = "roomformoreranch.com Administration"
 admin.site.site_title = "roomformoreranch.com"
